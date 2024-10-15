d27676761e9c8cf72af315d88a7add0eb755cc0a
anhthii <nguyendanganhthi247@gmail.com>
2024-10-15 13:15:26 +0700

    Attempt to setup hardhat deploy

--- pkg/pool-weighted/hardhat.config.ts
+++ pkg/pool-weighted/hardhat.config.ts
@@ -1,6 +1,9 @@
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
 import 'hardhat-ignore-warnings';
+import 'hardhat-deploy';
+import dotenv from 'dotenv';
+dotenv.config(); // Load environment variables from .env
 
 import { hardhatBaseConfig } from '@balancer-labs/v2-common';
 import { name } from './package.json';
@@ -12,9 +15,38 @@ import overrideQueryFunctions from '@balancer-labs/v2-helpers/plugins/overrideQu
 task(TASK_COMPILE).setAction(overrideQueryFunctions);
 
 export default {
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+    },
+  },
+  external: {
+    contracts: [
+      {
+        artifacts: '../standalone-utils',
+      },
+    ],
+  },
   networks: {
     hardhat: {
+      chainId: 1337,
       allowUnlimitedContractSize: true,
+    },
+    // for testnet
+    baseSepolia: {
+      url: 'https://sepolia.base.org',
+      accounts: [process.env.PRIVATE_KEY as string],
+      // gasPrice: 1000000000,
+    },
+    base: {
+      url: 'https://mainnet.base.org',
+      accounts: [process.env.PRIVATE_KEY as string],
+      // gasPrice: 1000000000,
+    },
+    polygon: {
+      url: 'https://polygon-bor-rpc.publicnode.com',
+      accounts: [process.env.PRIVATE_KEY as string],
+      // gasPrice: 1000000000,
     },
   },
   solidity: {
