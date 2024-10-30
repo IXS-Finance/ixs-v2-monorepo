078accc91c9b75508aff1b0f84b7b70be0dcd197
thi-investax <thi@investax.io>
2024-10-30 11:38:19 +0700

    :Downgrade hardhad-deploy version

--- pkg/pool-weighted/hardhat.config.ts
+++ pkg/pool-weighted/hardhat.config.ts
@@ -5,11 +5,11 @@ import 'hardhat-deploy';
 import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables from .env
 
-import { hardhatBaseConfig } from '@balancer-labs/v2-common';
-import { name } from './package.json';
+import {hardhatBaseConfig} from '@balancer-labs/v2-common';
+import {name} from './package.json';
 
-import { task } from 'hardhat/config';
-import { TASK_COMPILE } from 'hardhat/builtin-tasks/task-names';
+import {task} from 'hardhat/config';
+import {TASK_COMPILE} from 'hardhat/builtin-tasks/task-names';
 import overrideQueryFunctions from '@balancer-labs/v2-helpers/plugins/overrideQueryFunctions';
 
 task(TASK_COMPILE).setAction(overrideQueryFunctions);
@@ -33,25 +33,25 @@ export default {
       allowUnlimitedContractSize: true,
     },
     // for testnet
-    baseSepolia: {
-      url: 'https://sepolia.base.org',
-      accounts: [process.env.PRIVATE_KEY as string],
-      // gasPrice: 1000000000,
-    },
-    base: {
-      url: 'https://mainnet.base.org',
-      accounts: [process.env.PRIVATE_KEY as string],
-      // gasPrice: 1000000000,
-    },
-    polygon: {
-      url: 'https://polygon-bor-rpc.publicnode.com',
-      accounts: [process.env.PRIVATE_KEY as string],
-      // gasPrice: 1000000000,
-    },
+    // baseSepolia: {
+    //   url: 'https://sepolia.base.org',
+    //   accounts: [process.env.PRIVATE_KEY as string],
+    //   // gasPrice: 1000000000,
+    // },
+    // base: {
+    //   url: 'https://mainnet.base.org',
+    //   accounts: [process.env.PRIVATE_KEY as string],
+    //   // gasPrice: 1000000000,
+    // },
+    // polygon: {
+    //   url: 'https://polygon-bor-rpc.publicnode.com',
+    //   accounts: [process.env.PRIVATE_KEY as string],
+    //   // gasPrice: 1000000000,
+    // },
   },
   solidity: {
     compilers: hardhatBaseConfig.compilers,
-    overrides: { ...hardhatBaseConfig.overrides(name) },
+    overrides: {...hardhatBaseConfig.overrides(name)},
   },
   warnings: hardhatBaseConfig.warnings,
 };
